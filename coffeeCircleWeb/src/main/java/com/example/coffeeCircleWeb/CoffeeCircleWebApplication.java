616d1c3539450b933e5c366807b53445eae3de92
sasori7611san <107899079+sasori7611san@users.noreply.github.com>
2025-01-13 00:13:00 +0900

    新規作成

---
+++ coffeeCircleWeb/src/main/java/com/example/coffeeCircleWeb/CoffeeCircleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeCircleWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeCircleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeCircleWebApplication.class, args);
+	}
+
+}
---
+++ coffeeCircleWeb/src/test/java/com/example/coffeeCircleWeb/CoffeeCircleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeCircleWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeCircleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
