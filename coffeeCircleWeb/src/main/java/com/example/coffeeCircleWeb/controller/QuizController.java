31bd30272173add1ed1adb5d61248a5808df8259
sasori7611san <107899079+sasori7611san@users.noreply.github.com>
2025-01-18 18:51:43 +0900

    QuizController.java作成

---
+++ coffeeCircleWeb/src/main/java/com/example/coffeeCircleWeb/controller/QuizController.java
@@ -0,0 +1,33 @@
+package com.example.coffeeCircleWeb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.coffeeCircleWeb.model.AnswerResult;
+import com.example.coffeeCircleWeb.model.Question;
+import com.example.coffeeCircleWeb.service.QuizService;
+
+@RestController
+@RequestMapping("/api/quiz")
+public class QuizController {
+	
+	@Autowired
+	private QuizService quizService;
+	
+	@GetMapping("/questions")
+	public List<Question> getQuestions(@RequestParam(defaultValue="3") int count){
+		return quizService.getRandomQuestions(count);
+	}
+	
+	@PostMapping("/answer")
+	public AnswerResult submitAnswer(@RequestParam int questionNumber, @RequestParam int userAnswer) {
+		boolean isCorrect = quizService.checkAnswer(questionNumber, userAnswer);
+		return new AnswerResult(isCorrect, isCorrect ? 10 : 0);
+	}
+}
